76316d1dcaaa6aa3032a9ba89f6ab7809fc9fae6
Hugo Herter <git@hugoherter.com>
2023-01-25 20:00:28 +0100

    Fix: Rename aleph-client -> aleph-sdk-python

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-    Setup file for aleph_client.
+    Setup file for aleph.sdk
     Use setup.cfg to configure your project.
 
     This file was generated with PyScaffold 3.2.1.
