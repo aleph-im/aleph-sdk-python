aleph-im/aleph-client

Internal: user session class
Problem: we often pass 2-3 objects to each function that represent
information on how to connect to the Aleph network:

* the user account
* the API server URL
* the aiohttp session.

This can be simplified to improve the user experience and reduce
the complexity of the SDK.

Solution: introduce a new state class: `UserSession`. This object
is now passed as the first parameter of every public function.
It is initialized with the user account and the API server URL.
It is in charge of configuring and managing the aiohttp session object.

The typical usage of the library now looks like this:

```
account = load_my_account()
api_server = "https://aleph.cloud"  # example
async with UserSession(account=account, api_server=api_server) as session:
  message, status = await create_post(
    session=session,
    ...
  )
```

This enables the following improvements:

* Less clutter in function signatures: all public SDK functions now
  have 1 or 2 fewer arguments.
* The API server URL is now managed when initializing the aiohttp
  session inside the user session object. Implementations can
  simply specify the endpoint URL. Ex:
  `f"{api_server}/api/v0/messages.json` can now be expressed
  as `"/api/v0/messages.json"`.

Breaking changes:

* The signatures of all public functions of the SDK have been
  modified. The user must now initialize the user session object
  and pass it as parameter.

tests/unit/test_synchronous_get.py
@@ -1,0 +1,20 @@
+from aleph_message.models import MessageType, MessagesResponse
+
+from aleph_client.conf import settings
+from aleph_client.synchronous import get_messages
+from aleph_client.types import Account
+from aleph_client.user_session import UserSession
+
+
+def test_get_posts(mock_account: Account):
+    with UserSession(account=mock_account, api_server=settings.API_HOST) as session:
+        response: MessagesResponse = get_messages(
+            session=session,
+            pagination=2,
+            message_type=MessageType.post,
+        )
+
+        messages = response.messages
+        assert len(messages) > 1
+        for message in messages:
+            assert message.type == MessageType.post

aleph-im/aleph-sdk-python

tests/unit/test_synchronous_get.py
@@ -1,0 +1,20 @@
+from aleph_message.models import MessageType, MessagesResponse
+
+from aleph_client.conf import settings
+from aleph_client.synchronous import get_messages
+from aleph_client.types import Account
+from aleph_client.user_session import UserSession
+
+
+def test_get_posts(ethereum_account: Account):
+    with UserSession(account=ethereum_account, api_server=settings.API_HOST) as session:
+        response: MessagesResponse = get_messages(
+            session=session,
+            pagination=2,
+            message_type=MessageType.post,
+        )
+
+        messages = response.messages
+        assert len(messages) > 1
+        for message in messages:
+            assert message.type == MessageType.post