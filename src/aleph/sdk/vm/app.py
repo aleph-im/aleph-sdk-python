155dd0a01a11000a06261426d4589548d8f56571
Mike Hukiewitz <mike.hukiewitz@robotter.ai>
2023-04-19 12:01:32 +0200

    Problem: @app.on_event("startup") would not be triggered on uvicorn startup
    Solution: Relay "lifespan" events from the AlephApp to the http_app

--- src/aleph/sdk/vm/app.py
+++ src/aleph/sdk/vm/app.py
@@ -79,6 +79,10 @@ class AlephApp:
                             raise ValueError("No send method specified")
 
                     return send_handler_result()
+        elif scope["type"] == "lifespan":
+            # Call the lifespan handler
+            if self.http_app:
+                await self.http_app(scope=scope, receive=receive, send=send)
         else:
             raise ValueError(f"Unknown scope type '{scope['type']}'")
 
