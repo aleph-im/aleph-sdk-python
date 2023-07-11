f01bd66847eff1977c1e8809e1854d3fdba70f96
Hugo Herter <git@hugoherter.com>
2023-07-11 13:48:10 +0200

    Fix: Scripts used renamed Dockerfile

--- scripts/build-and-test.sh
+++ scripts/build-and-test.sh
@@ -10,6 +10,6 @@ else
   DOCKER_COMMAND=docker
 fi
 
-$DOCKER_COMMAND build -t aleph-sdk-python -f docker/Dockerfile .
+$DOCKER_COMMAND build -t aleph-sdk-python -f docker/python-3.9.dockerfile .
 $DOCKER_COMMAND run -ti --rm --entrypoint /opt/venv/bin/pytest aleph-sdk-python /opt/aleph-sdk-python/ "$@"
 $DOCKER_COMMAND run -ti --rm --entrypoint /opt/venv/bin/mypy aleph-sdk-python /opt/aleph-sdk-python/src/ --ignore-missing-imports
