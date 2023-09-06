ba18628ab1a9064c7481dfff47e2a7e826f7ef24
mhh <mike.hukiewitz@robotter.ai>
2023-09-06 12:41:45 +0200

    format and fix types

--- src/aleph/sdk/models.py
+++ src/aleph/sdk/models.py
@@ -1,6 +1,6 @@
-from typing import List, Optional, Any, Dict, Union
+from typing import Any, Dict, List, Optional, Union
 
-from aleph_message.models import AlephMessage, BaseMessage, ItemHash, ChainRef
+from aleph_message.models import AlephMessage, BaseMessage, ChainRef, ItemHash
 from pydantic import BaseModel, Field
 
 
@@ -23,16 +23,25 @@ class Post(BaseMessage):
     A post is a type of message that can be updated. Over the get_posts API
     we get the latest version of a post.
     """
+
     hash: ItemHash = Field(description="Hash of the content (sha256 by default)")
-    original_item_hash: ItemHash = Field(description="Hash of the original content (sha256 by default)")
+    original_item_hash: ItemHash = Field(
+        description="Hash of the original content (sha256 by default)"
+    )
     original_signature: Optional[str] = Field(
         description="Cryptographic signature of the original message by the sender"
     )
-    original_type: str = Field(description="The original, user-generated 'content-type' of the POST message")
-    content: Dict[str, Any] = Field(description="The content.content of the POST message")
+    original_type: str = Field(
+        description="The original, user-generated 'content-type' of the POST message"
+    )
+    content: Dict[str, Any] = Field(
+        description="The content.content of the POST message"
+    )
     type: str = Field(description="The content.type of the POST message")
     address: str = Field(description="The address of the sender of the POST message")
-    ref: Optional[Union[str, ChainRef]] = Field(description="Other message referenced by this one")
+    ref: Optional[Union[str, ChainRef]] = Field(
+        description="Other message referenced by this one"
+    )
 
 
 class PostsResponse(PaginationResponse):
