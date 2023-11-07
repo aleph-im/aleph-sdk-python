7f1b06db84851de141b92bba33bcdd69135b65c6
1yam <40899431+1yam@users.noreply.github.com>
2023-11-07 14:23:02 +0100

    Fix Upload Test : ServerDisconnectErrors
    Solution: Mock AuthenticatedAlephHttpClient to remove server call

--- tests/unit/test_upload.py
+++ tests/unit/test_upload.py
@@ -1,39 +1,60 @@
 import hashlib
+from unittest.mock import AsyncMock, patch
 
 import pytest
-from aleph_message.models import AlephMessage, StoreMessage
+from aleph_message.models import StoreMessage
 from aleph_message.status import MessageStatus
 
-from aleph.sdk import AuthenticatedAlephHttpClient
 from aleph.sdk.chains.common import get_fallback_private_key
 from aleph.sdk.chains.ethereum import ETHAccount
 from aleph.sdk.types import StorageEnum
 
 
-@pytest.mark.asyncio
-async def test_upload_with_message():
-    pkey = get_fallback_private_key()
-    account = ETHAccount(private_key=pkey)
+@pytest.fixture
+def mock_authenticated_aleph_http_client():
+    with patch(
+        "aleph.sdk.AuthenticatedAlephHttpClient", autospec=True
+    ) as MockHttpClient:
+        pkey = get_fallback_private_key()
+        account = ETHAccount(private_key=pkey)
 
+        http_session = AsyncMock()
+        mock_client = MockHttpClient.return_value
+        mock_client.http_session = http_session
+        mock_client.account = account
+    return mock_client
+
+
+@pytest.mark.asyncio
+async def test_upload_with_message(mock_authenticated_aleph_http_client):
     content = b"Test pyaleph upload\n"
     file_hash = hashlib.sha256(content).hexdigest()
 
-    async with AuthenticatedAlephHttpClient(account=account, api_server=None) as client:
-        message, status = await client.create_store(
-            address=account.get_address(),
-            file_content=content,
-            storage_engine=StorageEnum.storage,
-            sync=True,
-        )
-        print(message, status)
+    message = AsyncMock()
+    message.content.item_hash = file_hash
+    status = MessageStatus.PROCESSED
+    mock_authenticated_aleph_http_client.create_store.return_value = (message, status)
 
-        assert status == MessageStatus.PROCESSED
-        assert message.content.item_hash == file_hash
+    mock_authenticated_aleph_http_client.download_file.return_value = content
 
-        server_content = await client.download_file(file_hash=file_hash)
-        assert server_content == content
+    mock_authenticated_aleph_http_client.get_message.return_value = message
 
-        server_message: AlephMessage = await client.get_message(
-            item_hash=message.item_hash, message_type=StoreMessage
-        )
-        assert server_message.content.item_hash == file_hash
+    message, status = await mock_authenticated_aleph_http_client.create_store(
+        address=mock_authenticated_aleph_http_client.account.get_address(),
+        file_content=content,
+        storage_engine=StorageEnum.storage,
+        sync=True,
+    )
+
+    assert status == MessageStatus.PROCESSED
+    assert message.content.item_hash == file_hash
+
+    server_content = await mock_authenticated_aleph_http_client.download_file(
+        file_hash=file_hash
+    )
+    assert server_content == content
+
+    server_message = await mock_authenticated_aleph_http_client.get_message(
+        item_hash=message.item_hash, message_type=StoreMessage
+    )
+    assert server_message.content.item_hash == file_hash
