9639c68f1a80944dc5dad49890a7d5f21c543dc1
mhh <mike.hukiewitz@robotter.ai>
2023-11-28 12:36:03 +0100

    Add additional tests for `extended_json_encoder` to prove timezone compatibility

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,3 +1,6 @@
+import datetime
+
+import pytest as pytest
 from aleph_message.models import (
     AggregateMessage,
     Chain,
@@ -8,6 +11,7 @@ from aleph_message.models import (
     ProgramMessage,
     StoreMessage,
 )
+from aleph_message.models.execution.environment import MachineResources
 
 from aleph.sdk.utils import enum_as_str, get_message_type_value
 
@@ -26,3 +30,101 @@ def test_enum_as_str():
     assert enum_as_str(Chain.ETH) == "ETH"
     assert enum_as_str(ItemType("inline")) == "inline"
     assert enum_as_str(ItemType.inline) == "inline"
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "message_type, content",
+    [
+        (
+            MessageType.aggregate,
+            {
+                "content": {"Hello": datetime.datetime.now()},
+                "key": "test",
+                "address": "0x1",
+                "time": 1.0,
+            },
+        ),
+        (
+            MessageType.aggregate,
+            {
+                "content": {"Hello": datetime.date.today()},
+                "key": "test",
+                "address": "0x1",
+                "time": 1.0,
+            },
+        ),
+        (
+            MessageType.aggregate,
+            {
+                "content": {"Hello": datetime.time()},
+                "key": "test",
+                "address": "0x1",
+                "time": 1.0,
+            },
+        ),
+        (
+            MessageType.aggregate,
+            {
+                "content": {"Hello": datetime.timedelta()},
+                "key": "test",
+                "address": "0x1",
+                "time": 1.0,
+            },
+        ),
+        (
+            MessageType.aggregate,
+            {
+                "content": {"Hello": datetime.datetime.now().astimezone()},
+                "key": "test",
+                "address": "0x1",
+                "time": 1.0,
+            },
+        ),
+        (
+            MessageType.aggregate,
+            {
+                "content": {"Hello": datetime.datetime.now().astimezone().isoformat()},
+                "key": "test",
+                "address": "0x1",
+                "time": 1.0,
+            },
+        ),
+        (
+            MessageType.aggregate,
+            {
+                "content": {"Hello": datetime.datetime.now().astimezone().timestamp()},
+                "key": "test",
+                "address": "0x1",
+                "time": 1.0,
+            },
+        ),
+        (
+            MessageType.aggregate,
+            {
+                "content": {
+                    "Hello": MachineResources(
+                        vcpus=1,
+                        memory=1024,
+                        seconds=1,
+                    )
+                },
+                "key": "test",
+                "address": "0x1",
+                "time": 1.0,
+            },
+        ),
+    ],
+)
+async def test_prepare_aleph_message(
+    mock_session_with_post_success, message_type, content
+):
+    # Call the function under test
+    async with mock_session_with_post_success as session:
+        message = await session._prepare_aleph_message(
+            message_type=message_type,
+            content=content,
+            channel="TEST",
+        )
+
+    assert message.content.dict() == content
