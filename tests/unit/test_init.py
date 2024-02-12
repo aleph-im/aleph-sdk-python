9074d9832e1077a549b4aad8cae49d688ca9d488
Mike Hukiewitz <70762838+MHHukiewitz@users.noreply.github.com>
2024-02-12 16:36:46 +0100

    Feature: Add Deprecation Message (#103)
    Problem: too strict aleph-message dependency
    
    Solution: loosen it to accept compatible versions to 0.4.2
    
    Co-authored-by: Hugo Herter <git@hugoherter.com>

--- tests/unit/test_init.py
+++ tests/unit/test_init.py
@@ -1,5 +1,29 @@
+import pytest
+
 from aleph.sdk import __version__
 
 
 def test_version():
     assert __version__ != ""
+
+
+def test_deprecation():
+    with pytest.raises(ImportError):
+        from aleph.sdk import AlephClient  # noqa
+
+    with pytest.raises(ImportError):
+        from aleph.sdk import AuthenticatedAlephClient  # noqa
+
+    with pytest.raises(ImportError):
+        from aleph.sdk import synchronous  # noqa
+
+    with pytest.raises(ImportError):
+        from aleph.sdk import asynchronous  # noqa
+
+    with pytest.raises(ImportError):
+        import aleph.sdk.synchronous  # noqa
+
+    with pytest.raises(ImportError):
+        import aleph.sdk.asynchronous  # noqa
+
+    from aleph.sdk import AlephHttpClient  # noqa
