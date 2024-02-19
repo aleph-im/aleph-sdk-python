010bdd012a8a60b4beb454db90b8136de63516c9
philogicae <38438271+philogicae@users.noreply.github.com>
2024-02-19 06:21:08 +0200

    Remove useless IntEnum

--- src/aleph/sdk/query/filters.py
+++ src/aleph/sdk/query/filters.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from typing import Dict, Iterable, Optional, Union
 from aleph_message.models import MessageType
 from ..utils import _date_field_to_timestamp, serialize_list, enum_as_str
-from enum import Enum, IntEnum
+from enum import Enum
 
 
 class SortBy(str, Enum):
