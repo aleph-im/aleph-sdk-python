c27b714dec158a68dec3957f07f0ef4eab97aee0
mhh <mike.hukiewitz@robotter.ai>
2024-03-29 16:23:33 +0100

    Fix formatting

--- src/aleph/sdk/chains/sol.py
+++ src/aleph/sdk/chains/sol.py
@@ -1,2 +1,4 @@
-raise DeprecationWarning("Module aleph.sdk.chains.sol is deprecated in favor of aleph.sdk.chains.solana")
+raise DeprecationWarning(
+    "Module aleph.sdk.chains.sol is deprecated in favor of aleph.sdk.chains.solana"
+)
 from aleph.sdk.chains.solana import *
