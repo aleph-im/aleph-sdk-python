758da790d6a160aa2979d2086ccfed995926e4b5
mhh <mike.hukiewitz@robotter.ai>
2024-03-29 16:28:31 +0100

    Fix formatting, again.

--- src/aleph/sdk/chains/sol.py
+++ src/aleph/sdk/chains/sol.py
@@ -1,4 +1,8 @@
 from warnings import warn
 
-warn("Module aleph.sdk.chains.sol is deprecated in favor of aleph.sdk.chains.solana", DeprecationWarning, stacklevel=2)
+warn(
+    "Module aleph.sdk.chains.sol is deprecated in favor of aleph.sdk.chains.solana",
+    DeprecationWarning,
+    stacklevel=2,
+)
 from aleph.sdk.chains.solana import *
