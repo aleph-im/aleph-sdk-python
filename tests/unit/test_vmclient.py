68600157d8962319cf35c53d3f666e13ff05fa88
1yam <lyam.gomes@epitech.eu>
2024-06-19 18:00:41 +0200

    Add: Unit test for {perform_operation, stop, reboot, erase, expire}

--- tests/unit/test_vmclient.py
+++ tests/unit/test_vmclient.py
@@ -22,3 +22,94 @@ async def test_notify_allocation():
         await vm_client.notify_allocation(vm_id=vm_id)
         assert m.requests
         await vm_client.session.close()
+
+@pytest.mark.asyncio
+async def test_perform_operation():
+    account = ETHAccount(private_key=b"0x" + b"1" * 30)
+    vm_id = ItemHash("cafecafecafecafecafecafecafecafecafecafecafecafecafecafecafecafe")
+    operation = "reboot"
+
+    with aioresponses() as m:
+        vm_client = VmClient(
+            account=account,
+            node_url="http://localhost",
+            session=aiohttp.ClientSession(),
+        )
+        m.post(f"http://localhost/control/machine/{vm_id}/{operation}", status=200, payload="mock_response_text")
+
+        status, response_text = await vm_client.perform_operation(vm_id, operation)
+        assert status == 200
+        assert response_text == '"mock_response_text"' # ' ' cause by aioresponses
+        await vm_client.session.close()
+
+@pytest.mark.asyncio
+async def test_stop_instance():
+    account = ETHAccount(private_key=b"0x" + b"1" * 30)
+    vm_id = ItemHash("cafecafecafecafecafecafecafecafecafecafecafecafecafecafecafecafe")
+
+    with aioresponses() as m:
+        vm_client = VmClient(
+            account=account,
+            node_url="http://localhost",
+            session=aiohttp.ClientSession(),
+        )
+        m.post(f"http://localhost/control/machine/{vm_id}/stop", status=200, payload="mock_response_text")
+
+        status, response_text = await vm_client.stop_instance(vm_id)
+        assert status == 200
+        assert response_text == '"mock_response_text"' # ' ' cause by aioresponses
+        await vm_client.session.close()
+
+@pytest.mark.asyncio
+async def test_reboot_instance():
+    account = ETHAccount(private_key=b"0x" + b"1" * 30)
+    vm_id = ItemHash("cafecafecafecafecafecafecafecafecafecafecafecafecafecafecafecafe")
+
+    with aioresponses() as m:
+        vm_client = VmClient(
+            account=account,
+            node_url="http://localhost",
+            session=aiohttp.ClientSession(),
+        )
+        m.post(f"http://localhost/control/machine/{vm_id}/reboot", status=200, payload="mock_response_text")
+
+        status, response_text = await vm_client.reboot_instance(vm_id)
+        assert status == 200
+        assert response_text == '"mock_response_text"' # ' ' cause by aioresponses
+        await vm_client.session.close()
+
+@pytest.mark.asyncio
+async def test_erase_instance():
+    account = ETHAccount(private_key=b"0x" + b"1" * 30)
+    vm_id = ItemHash("cafecafecafecafecafecafecafecafecafecafecafecafecafecafecafecafe")
+
+    with aioresponses() as m:
+        vm_client = VmClient(
+            account=account,
+            node_url="http://localhost",
+            session=aiohttp.ClientSession(),
+        )
+        m.post(f"http://localhost/control/machine/{vm_id}/erase", status=200, payload="mock_response_text")
+
+        status, response_text = await vm_client.erase_instance(vm_id)
+        assert status == 200
+        assert response_text == '"mock_response_text"' # ' ' cause by aioresponses
+        await vm_client.session.close()
+
+@pytest.mark.asyncio
+async def test_expire_instance():
+    account = ETHAccount(private_key=b"0x" + b"1" * 30)
+    vm_id = ItemHash("cafecafecafecafecafecafecafecafecafecafecafecafecafecafecafecafe")
+
+    with aioresponses() as m:
+        vm_client = VmClient(
+            account=account,
+            node_url="http://localhost",
+            session=aiohttp.ClientSession(),
+        )
+        m.post(f"http://localhost/control/machine/{vm_id}/expire", status=200, payload="mock_response_text")
+
+        status, response_text = await vm_client.expire_instance(vm_id)
+        assert status == 200
+        assert response_text == '"mock_response_text"' # ' ' cause by aioresponses
+        await vm_client.session.close()
