0b03df250ea869013ea8185822b13329705fbb59
philogicae <38438271+philogicae@users.noreply.github.com>
2025-02-18 18:12:48 +0200

    Add CRN_URL_FOR_PROGRAMS to conf

--- src/aleph/sdk/conf.py
+++ src/aleph/sdk/conf.py
@@ -81,6 +81,7 @@ class Settings(BaseSettings):
     VM_URL_PATH = "https://aleph.sh/vm/{hash}"
     VM_URL_HOST = "https://{hash_base32}.aleph.sh"
     IPFS_GATEWAY = "https://ipfs.aleph.cloud/ipfs/"
+    CRN_URL_FOR_PROGRAMS = "https://dchq.staging.aleph.sh/"
 
     # Web3Provider settings
     TOKEN_DECIMALS = 18
