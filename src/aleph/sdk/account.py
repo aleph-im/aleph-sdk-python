bb02aca692a60531cc64830764fc49ad4d514326
nesitor <amolinsdiaz@yahoo.es>
2025-04-16 17:44:17 +0200

    Fix: Increase superfluid fork dependency version (#214)

--- src/aleph/sdk/account.py
+++ src/aleph/sdk/account.py
@@ -28,6 +28,7 @@ chain_account_map: Dict[Chain, Type[T]] = {  # type: ignore
     Chain.BOB: EVMAccount,
     Chain.CYBER: EVMAccount,
     Chain.DOT: DOTAccount,
+    Chain.ECLIPSE: SVMAccount,
     Chain.ETH: ETHAccount,
     Chain.FRAXTAL: EVMAccount,
     Chain.LINEA: EVMAccount,
@@ -40,7 +41,6 @@ chain_account_map: Dict[Chain, Type[T]] = {  # type: ignore
     Chain.SONIC: EVMAccount,
     Chain.WORLDCHAIN: EVMAccount,
     Chain.ZORA: EVMAccount,
-    Chain.ECLIPSE: SVMAccount,
 }
 
 
