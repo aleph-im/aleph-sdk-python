4dfcf248418fb6f83361252e8e277c1a67d7f3a2
1yam <lyam.gomes@epitech.eu>
2025-09-02 12:59:17 +0200

    Fix: only mock the get_posts from client and use real client

--- tests/unit/services/test_voucher.py
+++ tests/unit/services/test_voucher.py
@@ -1,5 +1,6 @@
 from unittest.mock import AsyncMock, MagicMock, patch
 
+from aleph.sdk.client.http import AlephHttpClient
 import pytest
 from aleph_message.models import Chain
 
@@ -16,12 +17,15 @@ from ..conftest import (
 
 @pytest.mark.asyncio
 async def test_get_evm_vouchers(mock_post_response, make_mock_aiohttp_session):
-    mock_client = MagicMock()
-    mock_client.get_posts = AsyncMock(return_value=mock_post_response)
-    voucher_service = Vouchers(client=mock_client)
+    client = AlephHttpClient(api_server="http://localhost")
+
+    # Patch only the get_posts who is used to fetch voucher update for EVM
+    client.get_posts = AsyncMock(return_value=mock_post_response) 
+    voucher_service = Vouchers(client=client)
 
     session = make_mock_aiohttp_session(MOCK_METADATA)
 
+    # Here we patch the client sessions who gonna fetch the metdata of the NFT
     with patch("aiohttp.ClientSession", return_value=session):
         vouchers = await voucher_service.get_evm_vouchers(MOCK_ADDRESS)
 
@@ -32,12 +36,16 @@ async def test_get_evm_vouchers(mock_post_response, make_mock_aiohttp_session):
 
 @pytest.mark.asyncio
 async def test_get_solana_vouchers(make_mock_aiohttp_session):
-    mock_client = MagicMock()
-    voucher_service = Vouchers(client=mock_client)
+    client = AlephHttpClient(api_server="http://localhost")
+    voucher_service = Vouchers(client=client)
 
     registry_session = make_mock_aiohttp_session(MOCK_SOLANA_REGISTRY)
     metadata_session = make_mock_aiohttp_session(MOCK_METADATA)
 
+    # Here we patch the fetch of the registry made on 
+    #  https://api.claim.twentysix.cloud/v1/registry/solanna
+    # and we also patch the fetch of the metadata 
+    #  https://claim.twentysix.cloud/sbt/metadata/{}.json
     with patch(
         "aiohttp.ClientSession", side_effect=[registry_session, metadata_session]
     ):
@@ -52,9 +60,9 @@ async def test_get_solana_vouchers(make_mock_aiohttp_session):
 async def test_fetch_vouchers_by_chain_for_evm(
     mock_post_response, make_mock_aiohttp_session
 ):
-    mock_client = MagicMock()
-    mock_client.get_posts = AsyncMock(return_value=mock_post_response)
-    voucher_service = Vouchers(client=mock_client)
+    client = AlephHttpClient(api_server="http://localhost")
+    client.get_posts = AsyncMock(return_value=mock_post_response)
+    voucher_service = Vouchers(client=client)
 
     metadata_session = make_mock_aiohttp_session(MOCK_METADATA)
     with patch("aiohttp.ClientSession", return_value=metadata_session):
@@ -89,9 +97,9 @@ async def test_fetch_vouchers_by_chain_for_solana(make_mock_aiohttp_session):
 async def test_get_vouchers_detects_chain(
     make_mock_aiohttp_session, mock_post_response
 ):
-    mock_client = MagicMock()
-    mock_client.get_posts = AsyncMock(return_value=mock_post_response)
-    voucher_service = Vouchers(client=mock_client)
+    client = AlephHttpClient(api_server="http://localhost")
+    client.get_posts = AsyncMock(return_value=mock_post_response)
+    voucher_service = Vouchers(client=client)
 
     # EVM
     metadata_session = make_mock_aiohttp_session(MOCK_METADATA)
