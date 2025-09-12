a4dc3c7b2cd536270d1b108e4c2d2fe5c8df8957
1yam <lyam.gomes@epitech.eu>
2025-09-12 14:18:12 +0200

    Feature: allow to update CRN_LIST_URL from .env

--- src/aleph/sdk/conf.py
+++ src/aleph/sdk/conf.py
@@ -86,7 +86,7 @@ class Settings(BaseSettings):
 
     DNS_API: ClassVar[str] = "https://api.dns.public.aleph.sh/instances/list"
     CRN_URL_UPDATE: ClassVar[str] = "{crn_url}/control/machine/{vm_hash}/update"
-    CRN_LIST_URL: ClassVar[str] = "https://crns-list.aleph.sh/crns.json"
+    CRN_LIST_URL: str = "https://crns-list.aleph.sh/crns.json"
     CRN_VERSION_URL: ClassVar[str] = (
         "https://api.github.com/repos/aleph-im/aleph-vm/releases/latest"
     )
