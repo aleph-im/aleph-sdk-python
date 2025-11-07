ef82a59cf087bcf93926e27c860f017cfb7e562d
1yam <lyam.gomes@epitech.eu>
2025-11-07 14:26:14 +0100

    Refactor: account.py to be able to handle more Account type than AccountFromPrivateKey

--- src/aleph/sdk/account.py
+++ src/aleph/sdk/account.py
@@ -1,9 +1,10 @@
 import logging
 from pathlib import Path
-from typing import Dict, Optional, Type, TypeVar, Union
+from typing import Dict, Literal, Optional, Type, TypeVar, Union, overload
 
 from aleph_message.models import Chain
 from ledgereth.exceptions import LedgerError
+from typing_extensions import TypeAlias
 
 from aleph.sdk.chains.common import get_fallback_private_key
 from aleph.sdk.chains.ethereum import ETHAccount
@@ -13,12 +14,13 @@ from aleph.sdk.chains.substrate import DOTAccount
 from aleph.sdk.chains.svm import SVMAccount
 from aleph.sdk.conf import AccountType, load_main_configuration, settings
 from aleph.sdk.evm_utils import get_chains_with_super_token
-from aleph.sdk.types import AccountFromPrivateKey
+from aleph.sdk.types import AccountFromPrivateKey, HardwareAccount
 from aleph.sdk.wallets.ledger import LedgerETHAccount
 
 logger = logging.getLogger(__name__)
 
 T = TypeVar("T", bound=AccountFromPrivateKey)
+AccountLike: TypeAlias = Union["AccountFromPrivateKey", "HardwareAccount"]
 
 chain_account_map: Dict[Chain, Type[T]] = {  # type: ignore
     Chain.ARBITRUM: EVMAccount,
@@ -56,7 +58,7 @@ def load_chain_account_type(chain: Chain) -> Type[AccountFromPrivateKey]:
 
 def account_from_hex_string(
     private_key_str: str,
-    account_type: Optional[Type[T]],
+    account_type: Optional[Type[AccountFromPrivateKey]],
     chain: Optional[Chain] = None,
 ) -> AccountFromPrivateKey:
     if private_key_str.startswith("0x"):
@@ -78,7 +80,7 @@ def account_from_hex_string(
 
 def account_from_file(
     private_key_path: Path,
-    account_type: Optional[Type[T]],
+    account_type: Optional[Type[AccountFromPrivateKey]],
     chain: Optional[Chain] = None,
 ) -> AccountFromPrivateKey:
     private_key = private_key_path.read_bytes()
@@ -97,13 +99,60 @@ def account_from_file(
     return account
 
 
+@overload
+def _load_account(
+    private_key_str: str,
+    private_key_path: None = None,
+    account_type: Type[AccountFromPrivateKey] = ...,
+    chain: Optional[Chain] = None,
+) -> AccountFromPrivateKey: ...
+
+
+@overload
+def _load_account(
+    private_key_str: Literal[None],
+    private_key_path: Path,
+    account_type: Type[AccountFromPrivateKey] = ...,
+    chain: Optional[Chain] = None,
+) -> AccountFromPrivateKey: ...
+
+
+@overload
+def _load_account(
+    private_key_str: Literal[None],
+    private_key_path: Literal[None],
+    account_type: Type[HardwareAccount],
+    chain: Optional[Chain] = None,
+) -> HardwareAccount: ...
+
+
+@overload
 def _load_account(
     private_key_str: Optional[str] = None,
     private_key_path: Optional[Path] = None,
-    account_type: Optional[Type[AccountFromPrivateKey]] = None,
+    account_type: Optional[Type[AccountLike]] = None,
     chain: Optional[Chain] = None,
-) -> Union[AccountFromPrivateKey, LedgerETHAccount]:
-    """Load an account from a private key string or file, or from the configuration file."""
+) -> AccountLike: ...
+
+
+def _load_account(
+    private_key_str: Optional[str] = None,
+    private_key_path: Optional[Path] = None,
+    account_type: Optional[Type[AccountLike]] = None,
+    chain: Optional[Chain] = None,
+) -> AccountLike:
+    """Load an account from a private key string or file, or from the configuration file.
+
+    This function can return different types of accounts based on the input:
+    - AccountFromPrivateKey: When a private key is provided (string or file)
+    - HardwareAccount: When config has AccountType.HARDWARE and a Ledger device is connected
+
+    The function will attempt to load an account in the following order:
+    1. From provided private key string
+    2. From provided private key file
+    3. From Ledger device (if config.type is HARDWARE)
+    4. Generate a fallback private key
+    """
 
     config = load_main_configuration(settings.CONFIG_FILE)
     default_chain = settings.DEFAULT_CHAIN
@@ -129,15 +178,21 @@ def _load_account(
 
     # Loads private key from a string
     if private_key_str:
-        return account_from_hex_string(private_key_str, account_type, chain)
+        return account_from_hex_string(private_key_str, None, chain)
+
     # Loads private key from a file
     elif private_key_path and private_key_path.is_file():
-        return account_from_file(private_key_path, account_type, chain)
-    elif config and config.address and config.type == AccountType.EXTERNAL:
-        logger.debug("Using remote account")
+        return account_from_file(private_key_path, account_type, chain)  # type: ignore
+    elif config and config.address and config.type == AccountType.HARDWARE:
+        logger.debug("Using ledger account")
         try:
             ledger_account = LedgerETHAccount.from_address(config.address)
             if ledger_account:
+                # Connect provider to the chain
+                # Only valid for EVM chain sign we sign TX using device
+                # and then use Superfluid logic to publish it to BASE / AVAX
+                if chain:
+                    ledger_account.connect_chain(chain)
                 return ledger_account
         except LedgerError as e:
             logger.warning(f"Ledger Error : {e.message}")
@@ -148,6 +203,6 @@ def _load_account(
 
     # Fallback: config.path if set, else generate a new private key
     new_private_key = get_fallback_private_key()
-    account = account_from_hex_string(bytes.hex(new_private_key), account_type, chain)
+    account = account_from_hex_string(bytes.hex(new_private_key), None, chain)
     logger.info(f"Generated fallback private key with address {account.get_address()}")
     return account
